e3c317da6d5067b505beb88278d476c252925201
Muqi <kepler22d@163.com>
2024-12-21 07:37:19 -0800

    Update Readme, KruskalMST.cpp

--- beforeNeetCode/IX.kruskalMST.cpp
+++ beforeNeetCode/IX.kruskalMST.cpp
@@ -4,39 +4,83 @@ using namespace std;
 
 typedef pair<int, int> Edge;
 
+struct FullEdge{
+    int u, v, weight;
+};
+
+/*bool compareEdges(const FullEdge &a, const FullEdge &b){
+    return a.weight < b.weight;
+}*/
+
 class DisjointSet{
 private:
-    vector<int> rank;
+    vector<int> parent, rank;
 
 public:
     DisjointSet(int n){
         rank.resize(n, 0);
+        parent.resize(n, -1);
     }
 
-    int find(int u){
-        int x = u;
-        while(rank[x] > 0){x = rank[x];}
-        return x;
+    int find(int i){
+        if(parent[i] == -1){return i;}
+        return parent[i] = find(parent[i]);
     }
 
     void unionSet(int u, int v){
-        if(rank[u] < rank[v]){
-            rank[u] += rank[v];
-            rank[v] = u;
-        }
-        else{
-            rank[v] += rank[u];
-            rank[u] = v;
+        int rootX = find(u);
+        int rootY = find(v);
+        if(rootX != rootY ){
+            if(rank[rootX] < rank[rootY]){parent[rootX] = rootY;}
+            else if(rank[rootX] > rank[rootY]){parent[rootY] = rootX;}
+            else{
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
         }
     }
 };
 
-void kruskalsAlgo(vector<vector<int>> &graph, int n){
-    int i = 0;
-    while(i < n - 1){
-        int min = INT_MAX;
-        for(int j = 0; j < ){}
+void kruskalsAlgo(vector<vector<Edge>> &graph, int n){
+    vector<FullEdge> edges;
+
+    // Convert adjacency list to a list of edges
+    for(int u = 0; u < n; u++){
+        for(const auto & edge : graph[u]){
+            int w = edge.first;
+            int v = edge.second;
+            if(u < v){edges.push_back({u, v, w});} // Avoid duplicate edges in an undirected graph
+        }
     }
+
+    // Sort edges by weight
+    //sort(edges.begin(), edges.end(), compareEdges);
+    sort(edges.begin(), edges.end(), [](const FullEdge &a, const FullEdge &b) {
+        return a.weight < b.weight;
+    });
+    
+    DisjointSet set(n);
+    vector<FullEdge> MST;
+    int mstW = 0;
+
+    for(const auto &edge : edges){
+        int u = edge.u;
+        int v = edge.v;
+        int w = edge.weight;
+        if(set.find(u) != set.find(v)){
+            set.unionSet(u, v);
+            MST.push_back(edge);
+            mstW += w;
+        }
+        if(MST.size() == n - 1){break;}
+    }
+
+    // Print the MST
+    cout << "Edge\tWeight\n";
+    for (const auto& edge : MST) {
+        cout << edge.u << " - " << edge.v << "\t" << edge.weight << "\n";
+    }
+    cout << "Total Weight of MST: " << mstW << "\n";
 }
 
 int main() {
