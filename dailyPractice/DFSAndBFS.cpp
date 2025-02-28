a83c05b607fc90ff7ed609112981c84df62a349d
Muqi <kepler22d@163.com>
2025-02-28 09:59:04 -0800

    Daily Practice Feb 28 2025

--- dailyPractice/DFSAndBFS.cpp
+++ dailyPractice/DFSAndBFS.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Graph {
 private:
     int V;
-    vector<vector<pair<int, int>>> adj; // weighted graph
+    vector<vector<pair<int, int>>> adj;
 
 public:
     Graph(int vertices){
@@ -17,37 +17,36 @@ public:
 
     void addEdges(int u, int v, int w){
         adj[u].push_back({v, w});
-        adj[v].push_back({u, w}); // undirected graph
-    }
-
-    void dfs_helper(int u, vector<int> &visit){
-        visit[u] = 1;
-        cout << u << " ";
-        for(const auto&nei : adj[u]){
-            if(!visit[nei.first]){
-                dfs_helper(nei.first, visit);
-            }
-        }
+        adj[v].push_back({u, w});
     }
 
     void dfs(int start){
         cout << "DFS starts: " << endl;
         vector<int> visit(V, 0);
-        dfs_helper(0, visit);
+        dfs_helper(start, visit);
         cout << endl;
+    }
+
+    void dfs_helper(int node, vector<int> &visit){
+        if(visit[node]){return;}
+        cout << node << " ";
+        visit[node] = 1;
+        for(const auto &nei : adj[node]){
+            dfs_helper(nei.first, visit);
+        }
     }
 
     void bfs(int start){
         cout << "BFS starts: " << endl;
-        queue<int> q;
         vector<int> visit(V, 0);
-        q.push(start);
+        queue<int> q;
         visit[start] = 1;
+        q.push(start);
         while(!q.empty()){
-            int u = q.front();
+            int node = q.front();
             q.pop();
-            cout << u << " ";
-            for(const auto&nei : adj[u]){
+            cout << node << " ";
+            for(const auto & nei : adj[node]){
                 if(!visit[nei.first]){
                     visit[nei.first] = 1;
                     q.push(nei.first);
@@ -57,11 +56,11 @@ public:
         cout << endl;
     }
 
-    void printGraph() {
-        for (int i = 0; i < V; i++) {
+    void printGraph(){
+        for(int i = 0; i < V; i++){
             cout << "Node " << i << ": ";
-            for (auto &neighbor : adj[i]) {
-                cout << "(" << neighbor.first << ", weight: " << neighbor.second << ") ";
+            for(const auto& nei : adj[i]){
+                cout << "(" << nei.first << ", weight: " << nei.second << ")";
             }
             cout << endl;
         }
@@ -85,6 +84,22 @@ int main() {
     // Performing DFS and BFS
     g.dfs(0);
     g.bfs(0);
+
+    /*Expected Output: 
+    Node 0: (1, weight: 4) (2, weight: 2)
+    Node 1: (0, weight: 4) (3, weight: 6) (4, weight: 3)
+    Node 2: (0, weight: 2) (5, weight: 8) (6, weight: 5)
+    Node 3: (1, weight: 6)
+    Node 4: (1, weight: 3)
+    Node 5: (2, weight: 8)
+    Node 6: (2, weight: 5)
+
+    DFS starts:
+    0 1 3 4 2 5 6
+
+    BFS starts:
+    0 1 2 3 4 5 6
+    */
 
     return 0;
 }
--- dailyPractice/mergeSort.cpp
+++ dailyPractice/mergeSort.cpp
@@ -10,12 +10,8 @@ void merge(vector<int> &nums, int low, int mid, int high){
         if(nums[l] < nums[r]){temp[k++] = nums[l++];}
         else{temp[k++] = nums[r++];}
     }
-    while(l <= mid){
-        temp[k++] = nums[l++];
-    }
-    while(r <= high){
-        temp[k++] = nums[r++];
-    }
+    while(l <= mid){temp[k++] = nums[l++];}
+    while(r <= high){temp[k++] = nums[r++];}
     for(int i = low; i <= high; i++){
         nums[i] = temp[i - low];
     }
@@ -31,7 +27,7 @@ void mergeSort(vector<int> &nums, int low, int high){
 }
 
 void mergeSortI(vector<int> &nums){
-    for(int i = 1; i < nums.size(); i *= 2){
+    for(int i = 1; i < nums.size(); i*= 2){
         for(int l = 0; l < nums.size() - 1; l += 2 * i){
             int mid = l + i - 1;
             int r = min(static_cast<int>(nums.size() - 1), l + 2 * i - 1);
