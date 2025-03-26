f7b8a5c331581a9827e0af72f61b8976fbf34854
Muqi <kepler22d@163.com>
2025-03-26 11:58:33 -0700

    Daily Practice Mar 26 2025 :(

--- dailyPractice/KruskalAlgo.cpp
+++ dailyPractice/KruskalAlgo.cpp
@@ -30,10 +30,10 @@ public:
 };
 
 void KruskalAlgo(vector<tuple<int, int, int>> &edges, int n){
-    DisjointSet set(n);
-    sort(edges.begin(), edges.end(), [](const tuple<int ,int, int> &a, const tuple<int, int, int> &b){
+    sort(edges.begin(), edges.end(), [](const tuple<int, int, int> &a, tuple<int, int, int> &b){
         return get<2>(a) < get<2>(b);
     });
+    DisjointSet set(n);
     vector<tuple<int, int, int>> inMST;
     for(const auto &[u, v, w] : edges){
         if(set.find(u) != set.find(v)){
@@ -42,7 +42,7 @@ void KruskalAlgo(vector<tuple<int, int, int>> &edges, int n){
         }
         if(inMST.size() == n - 1){break;}
     }
-    for(const auto &edge : inMST){
+    for(const auto&edge : inMST){
         cout << get<0>(edge) << " - " << get<1>(edge) << " " << get<2>(edge) << endl;
     }
 }
