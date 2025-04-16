1049c0b3cca95757351eae42cf8b4309c0e2be76
Muqi <kepler22d@163.com>
2025-04-16 09:33:43 -0700

    MultiHeadAttention Apr 16 2025

--- dailyPractice/MultiHeadAttention.py
+++ dailyPractice/MultiHeadAttention.py
@@ -1,15 +1,15 @@
-import torch 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class ScaledDotProductAttention(nn.Module):
-    def __init__(self, temperature, dropout=0.1):
+class ScaledDotProductAttention(nn.Medule):
+    def __init__(self, temprature, dropout=0.1):
         super().__init__()
-        self.temerature = temperature
+        self.temperature = temprature
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, q, k, v, mask=None):
-        attn = torch.matmul(q, k.transpose(-2, -1)) / self.temerature
+        attn = torch.malmul(q, k.transpose(-2, -1)) / self.temperature
         if mask is not None:
             attn = attn.mask_filled(mask==0, -1e9)
         attn = attn - attn.max(dim=-1, keepdim=True).values
@@ -19,6 +19,7 @@ class ScaledDotProductAttention(nn.Module):
     
 class LayerNorm(nn.Module):
     def __init__(self, d_model, eps=1e-6):
+        super().__init__()
         self.eps = eps
         self.d_model = d_model
         self.gamma = nn.Parameter(torch.ones(d_model))
@@ -26,7 +27,7 @@ class LayerNorm(nn.Module):
 
     def forward(self, x):
         mean = x.mean(dim=-1, keepdim=True)
-        var = x.var(dim=-1, deepdim=True, unbias=False)
+        var = x.vaf(dim=-1, unbiased=False, keepdim=True)
         x_norm = (x - mean) / torch.sqrt(var + self.eps)
         return self.gamma.unsqueeze(0).unsqueeze(0) * x_norm + self.beta.unsqueeze(0).unsqueeze(0)
     
@@ -43,28 +44,24 @@ class MultiHeadAttention(nn.Module):
         self.w_v = nn.Linear(d_model, n_head * d_v, bias=False)
         self.fc = nn.Linear(n_head * d_v, d_model, bias=False)
 
-        self.attention = ScaledDotProductAttention(temperature=d_k ** 0.5)
+        self.attention = ScaledDotProductAttention(temprature=d_k ** 0.5)
         self.dropout = nn.Dropout(dropout)
-        self.layer_norm = LayerNorm(d_model=d_model)
-
+        self.laryer_norm = LayerNorm(d_model=d_model)
+    
     def forward(self, q, k, v, mask=None):
-        batch_size, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
         d_model, d_k, d_v, n_head = self.d_model, self.d_k, self.d_v, self.n_head
+        batch_size, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
 
         residual = q
 
         q = self.w_q(q).view(batch_size, len_q, n_head, d_k)
         k = self.w_k(k).view(batch_size, len_k, n_head, d_k)
-        v = self.w_v(v).view(batch_size, len_v, n_head, d_k)
+        v = self.w_v(v).view(batch_size, len_v, n_head, d_v)
 
         q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
 
-        if mask is not None:
-            mask = mask.unsqueeze(1)
-
         output, attn = self.attention(q, k, v, mask=mask)
-        output.transpose(1, 2).contiguous().view(batch_size, len_q, -1)
         output = self.dropout(self.fc(output))
-        output += q
-        output = self.layer_norm(output)
+        output += residual
+        output = self.laryer_norm(output)
         return output, attn
