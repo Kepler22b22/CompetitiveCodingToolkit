6146fbfa11badd9d74d926757cd2f2f4fe2ffcae
Muqi <kepler22d@163.com>
2025-05-07 11:20:54 -0700

    MultiHeadAttention May 7 2025

--- dailyPractice/MultiHeadAttention.py
+++ dailyPractice/MultiHeadAttention.py
@@ -7,7 +7,7 @@ class ScaledDotProduct(nn.Module):
         super().__init__()
         self.temperature = temperature
         self.dropout = nn.Dropout(dropout)
-        
+
     def forward(self, q, k, v, mask=None):
         attn = torch.matmul(q, k.transpose(-2, -1)) / self.temperature
         attn = attn - attn.max(dim=-1, keepdim=True).values
@@ -16,7 +16,7 @@ class ScaledDotProduct(nn.Module):
         attn = self.dropout(F.softmax(attn, dim=-1))
         output = torch.matmul(attn, v)
         return output, attn
-    
+
 class LayerNorm(nn.Module):
     def __init__(self, d_model, eps=1e-6):
         super().__init__()
@@ -29,7 +29,7 @@ class LayerNorm(nn.Module):
         var = x.var(dim=-1, unbiased=False, keepdim=True)
         x_norm = (x - mean) / torch.sqrt(var + self.eps)
         return self.gamma * x_norm + self.beta
-
+    
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_model, d_k, d_v, n_head, dropout=0.1):
         super().__init__()
@@ -48,20 +48,20 @@ class MultiHeadAttention(nn.Module):
         self.layer_norm = LayerNorm(d_model=d_model)
 
     def forward(self, q, k, v, mask=None):
-        d_model, d_k, d_v, n_head = self.d_model, self.d_k, self.d_v, self.n_head
         batch_size, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
+        d_model, d_k, d_v, n_head = self.d_model, self.d_k, self.d_v, self.n_head
 
         residual = q
 
-        w_q = self.w_q(q).view(batch_size, len_q, n_head, d_k)
-        w_k = self.w_k(k).view(batch_size, len_k, n_head, d_k)
-        w_v = self.w_v(v).view(batch_size, len_v, n_head, d_v)
+        q = self.w_q(q).view(batch_size, len_q, n_head, d_k)
+        k = self.w_k(k).view(batch_size, len_k, n_head, d_k)
+        v = self.w_v(v).view(batch_size, len_v, n_head, d_v)
 
-        q, k, v = w_q.transpose(1, 2), w_k.transpose(1, 2), w_v.transpose(1, 2)
+        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
 
         if mask is not None:
             mask = mask.unsqueeze(1)
-
+        
         output, attn = self.attention(q, k, v, mask=mask)
         output = output.transpose(1, 2).contiguous().view(batch_size, len_q, -1)
         output = self.dropout(self.fc(output))
