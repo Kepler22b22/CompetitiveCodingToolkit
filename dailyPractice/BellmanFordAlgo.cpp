6fc598a3cb7e49f21edb170ee3d0bece3d5c6600
Muqi <kepler22d@163.com>
2025-06-15 16:05:16 +0800

    Daily Practice Jun 15 2025

--- dailyPractice/BellmanFordAlgo.cpp
+++ dailyPractice/BellmanFordAlgo.cpp
@@ -19,7 +19,7 @@ void BellmanFordAlgo(vector<tuple<int, int, int>> &edges, int n){
     }
     for(const auto &[u, v, w] : edges){
         if(dist[u] != INT_MAX && dist[v] > dist[u] + w){
-            cout << "Nagetive cycle detected: " << endl;
+            cout << "Nagetive cycle detected. " << endl;
         }
     }
     for(int i = 0; i < n; i++){
--- dailyPractice/PrimAlgo.cpp
+++ dailyPractice/PrimAlgo.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
 #include <queue>
+#include <unordered_set>
 
 using namespace std;
 
@@ -9,8 +9,8 @@ void primsAlgo(vector<vector<pair<int, int>>> &edges, int n){
     vector<int> parent(n, -1), key(n, INT_MAX);
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     unordered_set<int> visit;
-    key[0] = 0;
     pq.push({0, 0});
+    key[0] = 0;
     while(!pq.empty()){
         int u = pq.top().second;
         pq.pop();
@@ -18,14 +18,14 @@ void primsAlgo(vector<vector<pair<int, int>>> &edges, int n){
         visit.insert(u);
         for(const auto &edge : edges[u]){
             if(!visit.count(edge.first) && key[edge.first] > edge.second){
+                pq.push({edge.second, edge.first});
                 key[edge.first] = edge.second;
                 parent[edge.first] = u;
-                pq.push({edge.second, edge.first});
             }
         }
     }
     cout << "Edge\tWeight" << endl;
-    for(int i = 1; i < n; i++){
+    for(int i = 1; i < n ; i++){
         cout << parent[i] << " - " << i << " " << key[i] << endl;
     }
 }
