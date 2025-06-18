7e65a65f10d928e34cdb5d6159831355d91eac9b
Muqi <kepler22d@163.com>
2025-06-18 16:21:45 +0800

    Daily Practice Jun 18 2025

--- dailyPractice/TriesI.I.cpp
+++ dailyPractice/TriesI.I.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -23,7 +23,7 @@ public:
     void insert(string word){
         TrieNode *cur = root;
         for(char ch : word){
-            if(!cur->children.count(ch)){
+            if(!cur->children[ch]){
                 cur->children[ch] = new TrieNode();
             }
             cur = cur->children[ch];
