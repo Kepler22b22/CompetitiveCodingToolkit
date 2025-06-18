e9426ca6a205f93f339c257735644fc1b21e2928
Muqi <kepler22d@163.com>
2025-06-18 16:56:22 +0800

    Daily Practice Jun 18 2025

--- dailyPractice/DijkstraAlgo.cpp
+++ dailyPractice/DijkstraAlgo.cpp
@@ -17,8 +17,8 @@ vector<int> DijkstraAlgo(vector<vector<pair<int, int>>> &edges, int n, int start
         if(visit.count(u)){continue;}
         visit.insert(u);
         for(const auto &edge : edges[u]){
-            if(!visit.count(edge.first) && dist[edge.first] > edge.second + dist[u]){
-                dist[edge.first] = edge.second + dist[u];
+            if(!visit.count(edge.first) && dist[edge.first] > dist[u] + edge.second){
+                dist[edge.first] = dist[u] + edge.second;
                 pq.push({dist[edge.first], edge.first});
             }
         }
