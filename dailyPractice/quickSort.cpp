78513378abb974eca0950a320bd1bed81e076e6a
Muqi <kepler22d@163.com>
2025-06-23 09:37:11 +0800

    Daily Practice Jun 23 2025

--- dailyPractice/quickSort.cpp
+++ dailyPractice/quickSort.cpp
@@ -8,7 +8,7 @@ int partitionI(vector<int> &nums, int low, int high){
     while(l < r){
         while(l <= high && nums[l] <= pivot){l++;}
         while(r >= low && nums[r] > pivot){r--;}
-        if(l < r){swap(nums[l], nums[r]);}
+        if(l < r){swap(nums[r], nums[l]);}
     }
     swap(nums[low], nums[r]);
     return r;
@@ -16,29 +16,29 @@ int partitionI(vector<int> &nums, int low, int high){
 
 void quickSortI(vector<int> &nums, int low, int high){
     if(low < high){
-        int idx = partitionI(nums, low, high);
-        quickSortI(nums, low, idx - 1);
-        quickSortI(nums, idx + 1, high);
+        int i = partitionI(nums, low, high);
+        quickSortI(nums, low, i - 1);
+        quickSortI(nums, i + 1, high);
     }
 }
 
 int partitionII(vector<int> &nums, int low, int high){
     int pivot = nums[high], l = low - 1;
-    for(int r = low; l <= high; i++){
+    for(int r = low; r <= high; r++){
         if(nums[r] < pivot){
             l++;
             swap(nums[l], nums[r]);
         }
     }
-    swap(nums[high], nums[l + 1]);
+    swap(nums[l + 1], nums[high]);
     return l + 1;
 }
 
 void quickSortII(vector<int> &nums, int low, int high){
     if(low < high){
-        int idx = partitionI(nums, low, high);
-        quickSortI(nums, low, idx - 1);
-        quickSortI(nums, idx + 1, high);
+        int i = partitionII(nums, low, high);
+        quickSortII(nums, low, i - 1);
+        quickSortII(nums, i + 1, high);
     }
 }
 
