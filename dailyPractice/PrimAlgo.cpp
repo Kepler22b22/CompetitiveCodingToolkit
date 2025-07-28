e7929fbf9a7d7d27822c91cc48e868812851bb01
Muqi <kepler22d@163.com>
2025-07-28 12:38:12 +0800

    Daily Practice July 28 2025

--- dailyPractice/PrimAlgo.cpp
+++ dailyPractice/PrimAlgo.cpp
@@ -6,19 +6,19 @@
 using namespace std;
 
 void primsAlgo(vector<vector<pair<int, int>>> &edges, int n){
-    vector<int. parent(n, -1), key(n, INT_MAX);
+    vector<int> parent(n, -1), key(n, INT_MAX);
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     unordered_set<int> visit;
     key[0] = 0;
     pq.push({0, 0});
     while(!pq.empty()){
-        int u = pq.tp().second;
+        int u = pq.top().second;
         pq.pop();
         if(visit.count(u)){continue;}
         visit.insert(u);
         for(const auto &edge : edges[u]){
             if(!visit.count(edge.first) && key[edge.first] > edge.second){
-                pq.push(edge.second, edge.first);
+                pq.push({edge.second, edge.first});
                 key[edge.first] = edge.second;
                 parent[edge.first] = u;
             }
