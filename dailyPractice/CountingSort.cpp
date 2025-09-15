af46f9d7c1eff998ef995219a8150de69eafc3ac
Muqi <kepler22d@163.com>
2025-09-15 12:53:17 +0800

    Daily Practice Sep 15 2025

--- dailyPractice/CountingSort.cpp
+++ dailyPractice/CountingSort.cpp
@@ -10,7 +10,7 @@ void CountingSort(vector<int> &nums){
         count[num]++;
     }
     int idx = 0;
-    for(int i = 0; i < m + 1; i++){
+    for(int i = 0; i < nums.size(); i++){
         while(count[i]-- > 0){
             nums[idx++] = i;
         }
