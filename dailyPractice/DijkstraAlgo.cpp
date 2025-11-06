05db04251cf5de72d42e01d335fa690df1182072
Muqi <kepler22d@163.com>
2025-11-06 11:04:16 +0800

    Daily Practice Nov 6 2025

--- dailyPractice/DijkstraAlgo.cpp
+++ dailyPractice/DijkstraAlgo.cpp
@@ -9,8 +9,8 @@ vector<int> DijkstraAlgo(vector<vector<pair<int, int>>> &edges, int n, int start
     vector<int> dist(n, INT_MAX);
     dist[start] = 0;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({0, start});
     unordered_set<int> visit;
+    pq.push({0, start});
     while(!pq.empty()){
         int u = pq.top().second;
         pq.pop();
@@ -18,7 +18,7 @@ vector<int> DijkstraAlgo(vector<vector<pair<int, int>>> &edges, int n, int start
         visit.insert(u);
         for(const auto &edge : edges[u]){
             if(!visit.count(edge.first) && dist[edge.first] > edge.second + dist[u]){
-                dist[edge.first] = dist[u] + edge.second;
+                dist[edge.first] = edge.second + dist[u];
                 pq.push({dist[edge.first], edge.first});
             }
         }
