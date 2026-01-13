70ca6c59a45c6e40de5d7bab6b5fd7233f5265b7
Muqi <kepler22d@163.com>
2026-01-13 10:15:38 -0800

    Daily Practice Jan 13 2026

--- dailyPractice/BellmanFordAlgo.cpp
+++ dailyPractice/BellmanFordAlgo.cpp
@@ -3,28 +3,28 @@
 
 using namespace std;
 
-void BellmanFordAlgo(vector<tuple<int, int , int>> &edges, int n){
+void BellmanFordAlgo(vector<tuple<int, int, int>> &edges, int n){
     int superNode = n;
     vector<int> dist(n + 1, INT_MAX);
     dist[superNode] = 0;
     for(int i = 0; i < n; ++i){
-        edges.push_back({n, i, 0});
+        edges.push_back({superNode, i, 0});
     }
     for(int i = 0; i < n; ++i){
         for(const auto &[u, v, w] : edges){
-            if(dist[u] != INT_MAX && dist[v] > w + dist[u]){
-                dist[v] = w + dist[u];
+            if(dist[u] != INT_MAX && dist[v] > dist[u] + w){
+                dist[v] = dist[u] + w;
             }
         }
     }
     for(const auto &[u, v, w] : edges){
-        if(dist[u] != INT_MAX && dist[v] > w + dist[u]){
-            cout << "Negative cycle detected." << endl;
+        if(dist[u] != INT_MAX && dist[v] > dist[u] + w){
+            cout << "Nagetive cycle detected. " << endl;
         }
     }
     for(int i = 0; i < n; ++i){
-        if(dist[i] == INT_MAX){cout << "Node " << i << ": INT_MAX" << endl;}
-        else{cout << "Node " << i << ": " << dist[i] << endl;}
+        if(dist[i] == INT_MAX) cout << "Node " << i << ": INT_MAX" << endl;
+        else cout << "Node " << i << ": " << dist[i] << endl;
     }
 }
 
