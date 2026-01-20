866d64f1a1a6cc8369c27294150e13ac6e4f8c7a
Muqi <kepler22d@163.com>
2026-01-19 17:14:37 -0800

    Daily Practice Jan 16 2026, add cycle sort

---
+++ dailyPractice/CycleSort.cpp
@@ -0,0 +1,31 @@
+// [0, n] or [1, n]
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void CycleSort(vector<int> &nums){
+    int i = 0;
+    while(i < nums.size()){
+        int idx = nums[i] - 1;
+        if(nums[i] != nums[idx]) swap(nums[i], nums[idx]);
+        else i++;
+    }
+}
+
+void printArray(vector<int> &nums){
+    for (const auto &num : nums)
+        cout << num << " ";
+    cout << endl;
+}
+
+int main() {
+    vector<int> nums = { 3, 2, 4, 5, 1};
+    cout << "Before sorting array: \n";
+    printArray(nums);
+    CycleSort(nums);
+    cout << "Sorted array: \n";
+    printArray(nums);
+  return 0;
+
+}
