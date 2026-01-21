47c111b44f67223f30738f444b81463836b6277e
Muqi <kepler22d@163.com>
2026-01-21 09:38:33 -0800

    Daily Practice Jan 21 2026, update counting sort

--- dailyPractice/CountingSort.cpp
+++ dailyPractice/CountingSort.cpp
@@ -4,15 +4,14 @@
 using namespace std;
 
 void CountingSort(vector<int> &nums){
-    int m = *max_element(nums.begin(), nums.end());
-    vector<int> count(m + 1, 0);
-    for(const int &num : nums){
-        count[num]++;
-    }
+    int mx = *max_element(nums.begin(), nums.end());
+    int mn = *min_element(nums.begin(), nums.end());
+    vector<int> count(mx - mn + 1, 0);
+    for(const auto &num : nums) count[num - mn]++;
     int idx = 0;
-    for(int i = 0; i < nums.size(); ++i){
+    for(int i = 0; i <= mx - mn; ++i){
         while(count[i]-- > 0){
-            nums[idx++] = i;
+            nums[idx++] = i + mn;
         }
     }
 }
@@ -26,13 +25,21 @@ void printArray(vector<int> &nums){
 
 // Unstable: equal elements may not preserve original relative order (usually fine).
 int main() {
-    vector<int> nums = {2, 5, 3, 0, 2, 3, 0, 3};
+    vector<int> nums1 = {2, 5, 3, 0, 2, 3, 0, 3};
 
     cout << "Before: " << endl;
-    printArray(nums);
+    printArray(nums1);
     cout << "Counting Sort: " <<endl;
-    CountingSort(nums);
-    printArray(nums);
+    CountingSort(nums1);
+    printArray(nums1);
+
+    vector<int> nums2 = {-1, 0, 3, 7, 4, -2};
+
+    cout << "Before: " << endl;
+    printArray(nums2);
+    cout << "Counting Sort: " <<endl;
+    CountingSort(nums2);
+    printArray(nums2);
 
     return 0;
 }
