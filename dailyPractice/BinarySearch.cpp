19bde6a69cabcb52a22df7fa8934e9e5074bdd69
Muqi <kepler22d@163.com>
2026-02-02 10:31:41 -0800

    Daily Practice Feb 2 2026

--- dailyPractice/BinarySearch.cpp
+++ dailyPractice/BinarySearch.cpp
@@ -7,10 +7,10 @@ int findEqualOrGreater(vector<int> &nums, int target){
     int l = 0, r = nums.size() - 1, res = -1;
     while(l <= r){
         int mid = l + ((r - l) >> 1);
-        if(nums[mid] < target) l++;
+        if(nums[mid] < target) l = mid + 1;
         else{
             res = mid;
-            r--;
+            r = mid - 1;
         }
     }
     return res;
@@ -46,10 +46,10 @@ int findSmaller(vector<int> &nums, int target){
     int l = 0, r = nums.size() - 1, res = -1;
     while(l <= r){
         int mid = l + (r - l) / 2;
-        if(nums[mid] >= target) r--;
+        if(nums[mid] >= target) r = mid - 1;
         else{
             res = mid;
-            l++;
+            l = mid + 1;
         }
     }
     return res;
