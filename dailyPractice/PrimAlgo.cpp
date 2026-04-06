7fe76fe3793c36047e8c3bcbc5baef761ee38a28
Muqi <kepler22d@163.com>
2026-04-06 08:37:43 -0700

    Daily Practice Apr 6 2026

--- dailyPractice/PrimAlgo.cpp
+++ dailyPractice/PrimAlgo.cpp
@@ -9,8 +9,8 @@ void primsAlgo(vector<vector<pair<int, int>>> &edges, int n){
     vector<int> parent(n, -1), key(n, INT_MAX);
     unordered_set<int> visit;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    key[0] = 0;
     pq.push({0, 0});
+    key[0] = 0;
     while(!pq.empty()){
         int u = pq.top().second;
         pq.pop();
