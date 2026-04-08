d3782f2d446cf07a716b49a05af81f157d2026fb
Muqi <kepler22d@163.com>
2026-04-08 10:11:50 -0700

    Daily Practice Apr 8 2026

--- dailyPractice/mergeSort.cpp
+++ dailyPractice/mergeSort.cpp
@@ -7,7 +7,7 @@ void merge(vector<int> &nums, int low, int mid, int high){
     vector<int> temp(high - low + 1);
     int l = low, r = mid + 1, k = 0;
     while(l <= mid && r <= high){
-        if(nums[l] <= nums[r]) temp[k++] = nums[l++];
+        if(nums[l] < nums[r]) temp[k++] = nums[l++];
         else temp[k++] = nums[r++];
     }
     while(l <= mid) temp[k++] = nums[l++];
@@ -29,9 +29,9 @@ void mergeSort(vector<int> &nums, int low, int high){
 void mergeSortI(vector<int> &nums){
     for(int i = 1; i < nums.size(); i *= 2){
         for(int l = 0; l < nums.size() - i; l += 2 * i){
-            int mid = l + i - 1;
-            int r = max((int)nums.size() - 1, l + 2 * i - 1);
-            merge(nums, l, mid, r);
+            int m = l + i - 1;
+            int r = min((int)nums.size() - 1, l + 2 * i - 1);
+            merge(nums, l, m, r);
         }
     }
 }
