522bfed7e2baa4e3a8fd97bcf054c913ed0cfc34
Muqi <kepler22d@163.com>
2026-04-09 09:27:20 -0700

    Daily Practice Apr 9 2026

--- dailyPractice/CycleSort.cpp
+++ dailyPractice/CycleSort.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void CycleSort(vector<int> &nums){
     int i = 0;
     while(i < nums.size()){
-        if(nums[i] != nums[nums[i] - 1]) swap(nums[i], nums[nums[i] - 1]);
+        if(nums[i] != nums[nums[i] - 1])  swap(nums[i], nums[nums[i] - 1]);
         else i++;
     }
 }
