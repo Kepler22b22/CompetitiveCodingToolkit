1b6f1de0b32046e31671ebfa2d93b332a8d49ed8
Muqi <kepler22d@163.com>
2026-09-07 17:47:29 -0700

    Daily Practice Sep 7 2026

--- dailyPractice/CycleSort.cpp
+++ dailyPractice/CycleSort.cpp
@@ -1,14 +1,17 @@
-// [0, n] or [1, n], and appears exactly once.
 // O(n^2), but advantage: minimal writes, not minimal comparisons
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-void CycleSort(vector<int> &nums){
+// [0, n] or [1, n], and appears exactly once.
+void CycleSort(vector<int> &nums, int base) {   // base = 0 or 1
     int i = 0;
-    while(i < nums.size()){
-        if(nums[i] != nums[nums[i]]) swap(nums[i], nums[nums[i]]);
+    while (i < nums.size()) {
+        int correct = nums[i] - base;
+        if (correct < 0 || correct >= (int)nums.size()) throw invalid_argument("value out of range");
+        if (nums[i] != nums[correct]) swap(nums[i], nums[correct]);
         else i++;
     }
 }
@@ -20,10 +23,24 @@ void printArray(vector<int> &nums){
 }
 
 int main() {
-    vector<int> nums = { 3, 2, 4, 5, 1};
+    vector<int> nums = { 3, 2, 4, 5, 1, 0};
     cout << "Before sorting array: \n";
     printArray(nums);
-    CycleSort(nums);
+    CycleSort(nums, 0);
+    cout << "Sorted array: \n";
+    printArray(nums);
+
+    nums = {3, 2, 4, 5, 1};
+    cout << "Before sorting array: \n";
+    printArray(nums);
+    CycleSort(nums, 1);
+    cout << "Sorted array: \n";
+    printArray(nums);
+
+    nums = {3, 2, 4, 5};
+    cout << "Before sorting array: \n";
+    printArray(nums);
+    CycleSort(nums, 2);
     cout << "Sorted array: \n";
     printArray(nums);
   return 0;
